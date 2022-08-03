3954808fc1260d63d453a2ca2f13a0c5edcbfeac
Lucas Sabbatini Janot <54871727+Lucas-Sabbatini@users.noreply.github.com>
2022-08-03 11:18:31 -0300

    Delete forca-server directory

--- forca-server/landpage.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#search img").addEventListener("click",function (){
-    let gameInput = document.querySelector("#search input").value;
-    if (!gameInput) alert("Por favor preencha o campo de pesquisa");
-    else window.location.replace(`index.html$${gameInput}`);
-
-}, false);
